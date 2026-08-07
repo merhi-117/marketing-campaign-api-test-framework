701ac35dd7c4c779b37feb685a90db81c0855027
AlaaMerhi <merhi@my.yorku.ca>
2026-08-07 15:53:31 -0400

    refactor: add reusable REST Assured base test

--- src/test/java/com/alaamerhi/marketing/tests/CampaignCreationTest.java
+++ src/test/java/com/alaamerhi/marketing/tests/CampaignCreationTest.java
@@ -1,5 +1,6 @@
 package com.alaamerhi.marketing.tests;
 
+import com.alaamerhi.marketing.base.BaseApiTest;
 import io.restassured.response.Response;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,28 +10,11 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThan;
 
-class CampaignCreationTest {
-
-    private static final String BASE_URI = "http://localhost:3000";
-
-    private Response createCampaign(String name, String channel) {
-    String requestBody = """
-            {
-              "name": "%s",
-              "channel": "%s"
-            }
-            """.formatted(name, channel);
-
-    return given()
-        .baseUri(BASE_URI)
-        .contentType(ContentType.JSON)
-        .body(requestBody)
-    .when()
-        .post("/campaigns");
-    }
+class CampaignCreationTest extends BaseApiTest {
 
    @Test
     void shouldCreateCampaignWhenRequestIsValid() {
+
         String campaignName = "Automated Campaign";
 
         createCampaign(campaignName, "EMAIL")
@@ -53,7 +37,7 @@ class CampaignCreationTest {
                 """;
 
         given()
-            .baseUri(BASE_URI)
+            .spec(requestSpec)
             .contentType(ContentType.JSON)
             .body(requestBody)
         .when()
@@ -100,15 +84,4 @@ class CampaignCreationTest {
             .body("message", equalTo("A campaign with this name already exists"));
     }
 
-
-
-    @BeforeEach
-    void resetTestData() {
-        given()
-        .baseUri(BASE_URI)
-        .when()
-        .post("/test/reset")
-        .then()
-        .statusCode(204);
-    }
 }
--- src/test/java/com/alaamerhi/marketing/tests/CampaignLookupTest.java
+++ src/test/java/com/alaamerhi/marketing/tests/CampaignLookupTest.java
@@ -1,5 +1,6 @@
 package com.alaamerhi.marketing.tests;
 
+import com.alaamerhi.marketing.base.BaseApiTest;
 import io.restassured.response.Response;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,35 +9,7 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
-class CampaignLookupTest {
-
-    private static final String BASE_URI = "http://localhost:3000";
-
-    @BeforeEach
-    void resetTestData() {
-        given()
-            .baseUri(BASE_URI)
-        .when()
-            .post("/test/reset")
-        .then()
-            .statusCode(204);
-    }
-
-    private Response createCampaign(String name, String channel) {
-        String requestBody = """
-                {
-                  "name": "%s",
-                  "channel": "%s"
-                }
-                """.formatted(name, channel);
-
-        return given()
-            .baseUri(BASE_URI)
-            .contentType(ContentType.JSON)
-            .body(requestBody)
-        .when()
-            .post("/campaigns");
-    }
+class CampaignLookupTest extends BaseApiTest {
 
    @Test
     void shouldReturnCampaignWhenIdExists() {
@@ -48,7 +21,7 @@ class CampaignLookupTest {
                 .path("id");
 
         given()
-            .baseUri(BASE_URI)
+            .spec(requestSpec)
             .pathParam("id", campaignId)
         .when()
             .get("/campaigns/{id}")
@@ -64,7 +37,7 @@ class CampaignLookupTest {
     @Test
     void shouldReturnNotFoundWhenCampaignDoesNotExist() {
         given()
-            .baseUri(BASE_URI)
+            .spec(requestSpec)
             .pathParam("id", 999)
         .when()
             .get("/campaigns/{id}")
@@ -78,7 +51,7 @@ class CampaignLookupTest {
     @Test
     void shouldRejectCampaignIdWhenItIsNotANumber() {
         given()
-            .baseUri(BASE_URI)
+            .spec(requestSpec)
             .pathParam("id", "abc")
         .when()
             .get("/campaigns/{id}")
